05985386853529093016ba83836680ba53816847
Simon Wimmer <wimmers@in.tum.de>
2018-02-28 11:51:17 +0100

    Moved error monad

---
+++ src/Error.ml
@@ -0,0 +1,40 @@
+(* Error monad *)
+type error = string
+
+type 'a result =
+  | Result of 'a
+  | Error of error list
+
+let return m = Result m
+
+let bind m f = match m with
+    | Result x -> f x
+    | Error es -> Error es
+
+let combine2_gen (comb: 'a -> 'b -> 'c result) = function
+    | (Error e1, Error e2) -> Error (List.append e1 e2)
+    | (Error e, Result _) -> Error e
+    | (Result _, Error e) -> Error e
+    | (Result a, Result b) -> comb a b
+
+(* let combine2: ('a result * 'b result -> ('a * 'b) result) = combine2_gen (fun a b -> Result (a, b)) *)
+
+let combine2 = function
+    | (Error e1, Error e2) -> Error (List.append e1 e2)
+    | (Error e, Result _) -> Error e
+    | (Result _, Error e) -> Error e
+    | (Result a, Result b) -> Result (a, b)
+
+(* let (<|>) (x: 'a result) (y: 'b result): ('a * 'b) result  = combine2 (x, y) *)
+let (<|>) (x: 'a result) (y: 'b result): ('a * 'b) result = combine2 (x, y)
+let (>>=) = bind
+
+let rec combine: 'a result list -> 'a list result = function
+    | [] -> Result []
+    | (x :: xs) -> combine2_gen (fun (x: 'a) (xs: 'a list) -> Result (x :: xs)) (x, combine xs)
+
+let combine_map f xs = List.map f xs |> combine
+
+let map_errors f = function
+    | Error errors -> Error (List.map f errors)
+    | r -> r
